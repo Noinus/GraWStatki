6d56055a253066624da6e74e5143232ab98eb9d6
Noinus <kamilturko@gmail.com>
2018-03-25 23:15:19 +0200

    Added some things

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
-#include <iomanip>
-#include <time.h>
-#include <ui.h>
-
-using namespace std;
+#include <ai.h>
 
 //Game parameters
 bool t=0;               //1-ships are allowed to touch  0-ships are not allowed to touch
@@ -14,72 +10,25 @@ bool B[8][8];           //table of ships player B
 bool AS[8][8];          //table of shots player A
 bool BS[8][8];          //table of shots player B
 
-
-
-bool insertship(bool D[8][8], int S[4])
-{
-    cout << "enter new ship:" << endl;
-    string R;
-    cin >> R;
-    char param = R[0];
-    int x = R[1]-65;
-    int y = R[2]-49;
-    int l = R[3]-48;
-
-    cout <<"debug input:"<< param << " " << x << " " << y << " " << l << endl;
-    if(S[l-1]==0)
-        return 1;
-    if(x<0)
-        return 1;
-
-    if(param=='H')
-    {
-        if(x+l>8)
-            return 1;
-        for(int i=x;i<x+l;i++)
-            D[y][i]=1;
-        S[l-1]--;
-        return 0;
-    }
-    else if(param=='V')
-    {
-        if(y+l>8)
-            return 1;
-        for(int i=y;i<y+l;i++)
-            D[i][x]=1;
-        S[l-1]--;
-        return 0;
-    }
-    return 1;
-}
-
-void insert(bool D[8][8], int S[4])
-{
-    bool heh=1;
-    do{
-    heh=insertship(D,S);
-    if(heh)
-        cout << "Wrong input! Try again\n";
-    }while(heh);
-    cout <<"ships:"<< S[0] << " " << S[1] << " " << S[2] << " " << S[3] << endl;
-}
-
-void randomboard(bool T[][8])
-{
-    for(int i=0;i<8;i++)
-        for(int y=0;y<8;y++)
-            T[i][y]=rand()&2;
-}
-
 int main()
 {
     srand(time(NULL));
+    print(A,B,AS,BS);
+    /*
     cout << "time to insert!" << endl;
-    insert(A, Aas);
+    do
+    {
+        insert(A,t,Aas);
+        print(A,B,AS,BS);
+        if(Aas[0]==0 && Aas[1]==0 && Aas[2]==0 && Aas[3]==0)
+            break;
+    }while(true);
+    */
     //randomboard(A);
-    randomboard(B);
-    randomboard(AS);
-    randomboard(BS);
+    makeboard(A,B,AS,BS,t,Bas);
+    //randomboard(B);
+    //randomboard(AS);
+    //randomboard(BS);
     print(A,B,AS,BS);
     return 0;
 }
--- ui.h
+++ ui.h
@@ -5,7 +5,15 @@
 
 #include <iostream>
 #include <fstream>
+
 using namespace std;
+
+struct ship{
+    char p;
+    int x;
+    int y;
+    int l;
+};
 
 void print(bool A[8][8],bool B[8][8],bool AS[8][8],bool BS[8][8])
 {
@@ -56,6 +64,118 @@ void print(bool A[8][8],bool B[8][8],bool AS[8][8],bool BS[8][8])
         cout << endl;
     }
 }
+
+bool sanitycheck(bool t, bool A[8][8],int S[4], ship Ship)
+{
+    if(S[Ship.l-1]==0)
+        return 0;
+    if(Ship.x<0)
+        return 0;
+
+    if(Ship.p=='H')
+    {
+        if(Ship.x+Ship.l>8)
+            return 0;
+        for(int i=Ship.x;i<Ship.x+Ship.l;i++)
+        {
+            if(t)
+            {
+                if(A[Ship.y][i]==1)
+                    return 0;
+            }
+            else
+            {
+                if(A[Ship.y][i]==1 || A[Ship.y-1][i]==1 || A[Ship.y][i-1]==1 || A[Ship.y+1][i]==1 || A[Ship.y][i+1]==1)
+                    return 0;
+            }
+        }
+        return 1;
+    }
+    else if(Ship.p=='V')
+    {
+        if(Ship.y+Ship.l>8)
+            return 0;
+        for(int i=Ship.y;i<Ship.y+Ship.l;i++)
+        {
+            if(t)
+            {
+                if(A[i][Ship.x]==1)
+                    return 0;
+            }
+            else
+            {
+                if(A[i][Ship.x]==1 || A[i-1][Ship.x]==1  || A[i][Ship.x-1]==1 || A[i+1][Ship.x]==1  || A[i][Ship.x+1]==1 ==1)
+                    return 0;
+            }
+        }
+        return 1;
+    }
+    return 0;
+}
+
+void insertship(bool D[8][8], int S[4], ship &Ship)
+{
+    if(Ship.p=='H')
+    {
+        for(int i=Ship.x;i<Ship.x+Ship.l;i++)
+            D[Ship.y][i]=1;
+        S[Ship.l-1]--;
+        return;
+    }
+    else if(Ship.p=='V')
+    {
+        for(int i=Ship.y;i<Ship.y+Ship.l;i++)
+            D[i][Ship.x]=1;
+        S[Ship.l-1]--;
+        return;
+    }
+    return;
+}
+
+void insert(bool D[8][8], bool t,int S[4])
+{
+    ship Ship;
+    bool heh=1;
+    do{
+        cout << "Remaining ships:\n";
+        cout <<"ships:"<< S[0] << " " << S[1] << " " << S[2] << " " << S[3] << endl;
+        cout << "enter new ship:" << endl;
+        string R;
+        cin >> R;
+        char param = R[0];
+        int x = R[1]-65;
+        int y = R[2]-49;
+        int l = R[3]-48;
+
+        cout <<"debug input:"<< param << " " << x << " " << y << " " << l << endl;
+
+            if(param!='H' && param!='V')
+            {cout << "Wrong input! Try again\n";heh=1; continue;}
+            if(x<0 || x>8)
+            {cout << "Wrong input! Try again\n";heh=1; continue;}
+            if(y<0 || y>8)
+            {cout << "Wrong input! Try again\n";heh=1; continue;}
+            if(l<0 || l>4)
+            {cout << "Wrong input! Try again\n";heh=1; continue;}
+
+        Ship={param,x,y,l};
+
+        heh=sanitycheck(t,D,S,Ship);
+        if(!heh)
+            cout << "You cannot place ship here! Try again\n";
+    }while(!heh);
+    insertship(D,S,Ship);
+
+    cout <<"ships:"<< S[0] << " " << S[1] << " " << S[2] << " " << S[3] << endl;
+}
+
+void randomboard(bool T[][8])
+{
+    for(int i=0;i<8;i++)
+        for(int y=0;y<8;y++)
+            T[i][y]=rand()&2;
+}
+
 /*
 void load(string nazwa_pliku,bool A[8][8],bool B[8][8],bool AS[8][8],bool BS[8][8])
 {
